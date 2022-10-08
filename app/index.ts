302b0f9d85d633685212471c8bd2f35981288fbf
Chocobruh <olteanufabian99@gmail.com>
2022-10-08 16:35:00 +0300

    added nixie tubes for time

--- app/index.ts
+++ app/index.ts
@@ -1,16 +1,25 @@
 import Clock from "./Clock";
-import document from "document";
+import document, { DocumentModule } from "document";
 import DateDisplay from "./DateDisplay";
 import { display } from "display";
 
-const timeElement = document.getElementById("time") as TextElement;
-const dateElement = document.getElementById("date") as TextElement;
-const dayOfTheWeekElement = document.getElementById("dayOfTheWeek") as TextElement; 
+const timeElement = document.getElementById("divergence_meter").children.slice(2,8) as ImageElement[]; // keep only the last 6 nixie tubes for time
+const dateElement = document.getElementById("date") as ImageElement;
+const dayOfTheWeekElement = document.getElementById("dayOfTheWeek") as ImageElement; 
 
 const clock = new Clock();
 const dateDisplay = new DateDisplay();
 
-clock.clockCallback = (t) => timeElement.text = t;
-dateDisplay.dateCallback = (t) => dateElement.text = t;
-dateDisplay.dayOfTheWeekCallback = (t) => dayOfTheWeekElement.text = t;
+const clockMap = {
+
+};
+
+clock.clockCallback = (t) => {
+    t = t.replace(/:/g,''); 
+    for (let i = 0; i < timeElement.length; i++) {
+        timeElement[i].href = `../resources/Nixie tubes/${t[i]}.png`;
+    }    
+};
+//dateDisplay.dateCallback = (t) => dateElement.text = t;
+//dateDisplay.dayOfTheWeekCallback = (t) => dayOfTheWeekElement.text = t;
 
